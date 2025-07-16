9a6e2c828ac0783fedebba98369ecb12294c89c3
YutaKakiki <uta31501224@gmail.com>
2025-07-17 02:22:58 +0900

    ♻️ Refactor complete

--- completer/complete.go
+++ completer/complete.go
@@ -16,56 +16,31 @@ func NewCompleter(candidates *candidates) *Completer {
 	}
 }
 
-type pkgAndInput struct {
-	pkg   string
-	input string
-}
-
 func (c *Completer) Complete(input prompt.Document) []prompt.Suggest {
 	inputStr := input.Text
 	suggestions := make([]prompt.Suggest, 0)
+
+	if strings.Contains(inputStr, "&") {
+		isAmpersandInclude = true
+		inputStr = strings.ReplaceAll(inputStr, "&", "")
+	}
+
 	equalAndSpacePos, found := findEqualAndSpacePos(inputStr)
 	if found {
 		inputStr = inputStr[equalAndSpacePos+2:]
 		// . までは、packageの候補を表示する
 		if !strings.Contains(inputStr, ".") {
-			for _, pkg := range c.candidates.pkgs {
-				if strings.HasPrefix(string(pkg), inputStr) {
-					suggestions = append(suggestions, prompt.Suggest{
-						Text:        string(pkg),
-						DisplayText: string(pkg),
-						Description: "Package",
-					})
-				}
-			}
+			c.findAndAppendPackage(suggestions, inputStr)
 			return suggestions
 		}
 	}
 
 	if !strings.Contains(inputStr, ".") {
-		for _, pkg := range c.candidates.pkgs {
-			if strings.HasPrefix(string(pkg), inputStr) {
-				suggestions = append(suggestions, prompt.Suggest{
-					Text:        string(pkg),
-					DisplayText: string(pkg),
-					Description: "Package",
-				})
-			}
-		}
+		c.findAndAppendPackage(suggestions, inputStr)
 		return suggestions
 	}
 
-	for _, decl := range DeclVarInTerminalList {
-		for _, methodSet := range c.candidates.methods[pkgName(decl.Pkg)] {
-			if decl.Type == methodSet.receiverTypeName {
-				suggestions = append(suggestions, prompt.Suggest{
-					Text:        inputStr + methodSet.name + "()",
-					DisplayText: methodSet.name + "()",
-					Description: "Method: " + methodSet.description,
-				})
-			}
-		}
-	}
+	c.findAndAppendMethod(suggestions, inputStr)
 
 	pkgAndInput := buildPkgAndInput(inputStr)
 	findedSuggestions := c.findSuggestions(pkgAndInput)
@@ -76,6 +51,32 @@ func (c *Completer) Complete(input prompt.Document) []prompt.Suggest {
 
 func (c *Completer) findSuggestions(pai pkgAndInput) []prompt.Suggest {
 	var suggestions []prompt.Suggest
+	if isAmpersandInclude {
+		c.findAndAppendStruct(suggestions, pai)
+		return suggestions
+	}
+	c.findAndAppendPackage(suggestions, pai.input)
+	c.findAndAppendFunction(suggestions, pai)
+	c.findAndAppendVariable(suggestions, pai)
+	c.findAndAppendConstant(suggestions, pai)
+	c.findAndAppendStruct(suggestions, pai)
+
+	return suggestions
+}
+
+func (c *Completer) findAndAppendPackage(suggestions []prompt.Suggest, inputStr string) {
+	for _, pkg := range c.candidates.pkgs {
+		if strings.HasPrefix(string(pkg), inputStr) {
+			suggestions = append(suggestions, prompt.Suggest{
+				Text:        addAmpersand() + string(pkg),
+				DisplayText: string(pkg),
+				Description: "Package",
+			})
+		}
+	}
+}
+
+func (c *Completer) findAndAppendFunction(suggestions []prompt.Suggest, pai pkgAndInput) {
 	if funcSets, ok := c.candidates.funcs[pkgName(pai.pkg)]; ok {
 		for _, funcSet := range funcSets {
 			if strings.HasPrefix(funcSet.name, pai.input) {
@@ -87,7 +88,25 @@ func (c *Completer) findSuggestions(pai pkgAndInput) []prompt.Suggest {
 			}
 		}
 	}
+}
 
+func (c *Completer) findAndAppendMethod(suggestions []prompt.Suggest, inputStr string) {
+	for _, decl := range DeclVarRecords {
+		if (decl.Name + ".") == inputStr {
+			for _, methodSet := range c.candidates.methods[pkgName(decl.Pkg)] {
+				if decl.Type == methodSet.receiverTypeName {
+					suggestions = append(suggestions, prompt.Suggest{
+						Text:        inputStr + methodSet.name + "()",
+						DisplayText: methodSet.name + "()",
+						Description: "Method: " + methodSet.description,
+					})
+				}
+			}
+		}
+	}
+}
+
+func (c *Completer) findAndAppendVariable(suggestions []prompt.Suggest, pai pkgAndInput) {
 	if varSets, ok := c.candidates.vars[pkgName(pai.pkg)]; ok {
 		for _, varSet := range varSets {
 			if strings.HasPrefix(varSet.name, pai.input) {
@@ -99,6 +118,9 @@ func (c *Completer) findSuggestions(pai pkgAndInput) []prompt.Suggest {
 			}
 		}
 	}
+}
+
+func (c *Completer) findAndAppendConstant(suggestions []prompt.Suggest, pai pkgAndInput) {
 	if constSets, ok := c.candidates.consts[pkgName(pai.pkg)]; ok {
 		for _, constSet := range constSets {
 			if strings.HasPrefix(constSet.name, pai.input) {
@@ -110,18 +132,33 @@ func (c *Completer) findSuggestions(pai pkgAndInput) []prompt.Suggest {
 			}
 		}
 	}
-	if typeSets, ok := c.candidates.types[pkgName(pai.pkg)]; ok {
-		for _, typeSet := range typeSets {
-			if strings.HasPrefix(typeSet.name, pai.input) {
+}
+
+func (c *Completer) findAndAppendStruct(suggestions []prompt.Suggest, pai pkgAndInput) {
+	if structSets, ok := c.candidates.structs[pkgName(pai.pkg)]; ok {
+		for _, structSet := range structSets {
+			var field string
+			if len(structSet.fields) > 0 {
+				field += "{"
+				for _, name := range structSet.fields {
+					field += name + ": ,"
+				}
+				field = strings.TrimSuffix(field, ",") + "}"
+			}
+			if strings.HasPrefix(structSet.name, pai.input) {
 				suggestions = append(suggestions, prompt.Suggest{
-					Text:        pai.pkg + "." + typeSet.name,
-					DisplayText: typeSet.name,
-					Description: "Type: " + typeSet.description,
+					Text:        addAmpersand() + pai.pkg + "." + structSet.name + field,
+					DisplayText: structSet.name,
+					Description: "Struct: " + structSet.description,
 				})
 			}
 		}
 	}
-	return suggestions
+}
+
+type pkgAndInput struct {
+	pkg   string
+	input string
 }
 
 // {pkg名}. まで入力されている場合は、pkg名とその後の文字列を構造体にまとめる
