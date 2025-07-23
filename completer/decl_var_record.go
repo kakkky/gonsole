7fa22d0a31db625e8ee4876dfd33f10bb4c894c4
YutaKakiki <uta31501224@gmail.com>
2025-07-24 02:03:58 +0900

    ✨ Add func to DeclVarRecord

--- completer/decl_var_record.go
+++ completer/decl_var_record.go
@@ -7,3 +7,21 @@ type DeclVarRecord struct {
 }
 
 var DeclVarRecords = []DeclVarRecord{}
+
+// TODO: &つきで定義されたものはポインタ型の関数を列挙するようにしたい
+func StoreDeclVarRecord(pkg, name, typeName string) {
+	DeclVarRecords = append(DeclVarRecords, DeclVarRecord{
+		Pkg:  pkg,
+		Name: name,
+		Type: typeName,
+	})
+}
+
+func IsStoredReceiver(name string) bool {
+	for _, decl := range DeclVarRecords {
+		if decl.Name == name {
+			return true
+		}
+	}
+	return false
+}
