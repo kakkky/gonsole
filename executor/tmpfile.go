895882bb50f3b6681940e041da0c4d13ca572bcb
YutaKakiki <uta31501224@gmail.com>
2025-07-24 01:02:18 +0900

    ✨ If there are no other files in the tmp directory, it is now deleted entirely

--- executor/tmpfile.go
+++ executor/tmpfile.go
@@ -1,6 +1,7 @@
 package executor
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 )
@@ -11,20 +12,27 @@ func makeTmpMainFile() (string, func(), error) {
 	if err := os.Mkdir("tmp", 0755); err != nil && !os.IsExist(err) {
 		return "", nil, err
 	}
-	tmpDir, err := os.MkdirTemp("tmp", "gonsole")
+	gonsoleDir, err := os.MkdirTemp("tmp", "gonsole")
 	if err != nil {
 		return "", nil, err
 	}
-	if _, err := os.Create(filepath.Join(tmpDir, "main.go")); err != nil {
+	if _, err := os.Create(filepath.Join(gonsoleDir, "main.go")); err != nil {
 		return "", nil, err
 	}
-	tmpFilePath := filepath.Join(tmpDir, "main.go")
+	tmpFilePath := filepath.Join(gonsoleDir, "main.go")
 	if err := os.WriteFile(tmpFilePath, []byte(src), 0644); err != nil {
 		return "", nil, err
 	}
 	cleaner := func() {
 		os.Remove(tmpFilePath)
-		os.Remove(tmpDir)
+		os.RemoveAll(gonsoleDir)
+		entries, err := os.ReadDir("tmp")
+		if err != nil {
+			fmt.Printf("Failed to read tmp directory: %v\n", err)
+		}
+		if len(entries) == 0 {
+			os.Remove("tmp")
+		}
 	}
 	return tmpFilePath, cleaner, nil
 }
