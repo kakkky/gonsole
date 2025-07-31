89e8231ee1aaec68ad7c6496b3f2bd374968dff7
YutaKakiki <uta31501224@gmail.com>
2025-08-01 02:48:36 +0900

    ♻️ Refactored decls

--- decls/decl_entry.go
+++ decls/decl_entry.go
@@ -30,115 +30,150 @@ func (de *DeclEntry) Register(input string) error {
 		}
 	}
 	switch stmt := inputStmt.(type) {
+	// 短縮変数宣言の場合
 	case *ast.AssignStmt:
-		for i, expr := range stmt.Rhs {
-			switch rhs := expr.(type) {
+		for i, rhsExpr := range stmt.Rhs {
+			switch exprV := rhsExpr.(type) {
+			// 右辺がセレクタ式の場合
 			case *ast.SelectorExpr:
-				pkgName := rhs.X.(*ast.Ident).Name
+				pkgSelName := exprV.X.(*ast.Ident).Name
 				name := stmt.Lhs[i].(*ast.Ident).Name
-				varDecl := &declVar{
-					name: rhs.Sel.Name,
+				declVar := &declVar{
+					name: exprV.Sel.Name,
 				}
-				de.register(pkgName, name, *varDecl)
+				de.register(pkgSelName, name, *declVar)
+			// 右辺が複合リテラルの場合
 			case *ast.CompositeLit:
-				if selExpr, ok := rhs.Type.(*ast.SelectorExpr); ok {
-					pkgName := selExpr.X.(*ast.Ident).Name
-					structDecl := &declStruct{
-						typeName: selExpr.Sel.Name,
+				switch funExprV := exprV.Type.(type) {
+				// セレクタ式の場合
+				// 基本的にセレクタ式しか想定しない
+				case *ast.SelectorExpr:
+					pkgName := funExprV.X.(*ast.Ident).Name
+					declStruct := &declStruct{
+						// 型名を取得
+						typeName: funExprV.Sel.Name,
 					}
 					name := stmt.Lhs[i].(*ast.Ident).Name
-					de.register(pkgName, name, *structDecl)
+					de.register(pkgName, name, *declStruct)
 				}
+			// 右辺が演算子つきの場合
 			case *ast.UnaryExpr:
-				if rhs.Op == token.AND {
-					// & 演算子の場合
-					if compLit, ok := rhs.X.(*ast.CompositeLit); ok {
-						if selExpr, ok := compLit.Type.(*ast.SelectorExpr); ok {
-							pkgName := selExpr.X.(*ast.Ident).Name
-							structDecl := &declStruct{
-								typeName: selExpr.Sel.Name,
+				switch exprV.Op {
+				// & 演算子の場合
+				// (構造体をポインタ型で表現している場合）
+				case token.AND:
+					switch exprV.X.(type) {
+					// 複合リテラルの場合
+					case *ast.CompositeLit:
+						switch funExprV := exprV.X.(type) {
+						// セレクタ式の場合
+						case *ast.SelectorExpr:
+							pkgName := funExprV.X.(*ast.Ident).Name
+							declStruct := &declStruct{
+								typeName: funExprV.Sel.Name,
 							}
 							name := stmt.Lhs[i].(*ast.Ident).Name
-							de.register(pkgName, name, *structDecl)
+							de.register(pkgName, name, *declStruct)
 						}
 					}
 				}
+			// 右辺が関数呼び出しの場合
 			case *ast.CallExpr:
-				// 関数の戻り値を代入している場合
-				if selExpr, ok := rhs.Fun.(*ast.SelectorExpr); ok {
-					pkgName := selExpr.X.(*ast.Ident).Name
-					if de.IsRegisteredDecl(pkgName) {
-						pkgName = de.receiverTypePkgName(pkgName)
-						for i, lhsExpr := range stmt.Lhs {
+				switch funExprV := exprV.Fun.(type) {
+				case *ast.SelectorExpr:
+					// . 呼び出しの左側はまだパッケージ名か定義した変数かわからない
+					xName := funExprV.X.(*ast.Ident).Name
+					// 定義ずみの変数だったら、それはメソッド呼び出し
+					if de.IsRegisteredDecl(xName) {
+						declReceiver := xName
+						pkgName := de.receiverTypePkgName(declReceiver)
+						for j, lhsExpr := range stmt.Lhs {
 							methodDecl := &declMethod{
-								name:  selExpr.Sel.Name,
-								order: i,
+								name:  funExprV.Sel.Name,
+								order: j,
 							}
 							name := lhsExpr.(*ast.Ident).Name
 							de.register(pkgName, name, *methodDecl)
 						}
-					} else {
-						for i, lhsExpr := range stmt.Lhs {
-							funcDecl := &declFunc{
-								name:  selExpr.Sel.Name,
-								order: i,
-							}
-							name := lhsExpr.(*ast.Ident).Name
-							de.register(pkgName, name, *funcDecl)
-						}
 					}
+					// パッケージ名付きの関数呼び出し (pkg.Func())
+					pkgName := xName
+					for j, lhsExpr := range stmt.Lhs {
+						funcDecl := &declFunc{
+							name:  funExprV.Sel.Name,
+							order: j,
+						}
+						name := lhsExpr.(*ast.Ident).Name
+						de.register(pkgName, name, *funcDecl)
+					}
+					return nil
+
 				}
 			}
 		}
+	// 宣言の場合
 	case *ast.DeclStmt:
 		switch decl := stmt.Decl.(type) {
+		// 汎用的宣言の場合(const, var)
 		case *ast.GenDecl:
 			for _, spec := range decl.Specs {
-				if valSpec, ok := spec.(*ast.ValueSpec); ok {
-					for i, val := range valSpec.Values {
-						switch rhs := val.(type) {
+				switch specV := spec.(type) {
+				// 変数宣言
+				case *ast.ValueSpec:
+					for i, valExpr := range specV.Values {
+						switch valExprV := valExpr.(type) {
+						// セレクタ式の場合
 						case *ast.SelectorExpr:
-							pkgName := rhs.X.(*ast.Ident).Name
-							varDecl := &declVar{
-								name: rhs.Sel.Name,
+							pkgName := valExprV.X.(*ast.Ident).Name
+							declVar := &declVar{
+								name: valExprV.Sel.Name,
 							}
-							name := valSpec.Names[i].Name
-							de.register(pkgName, name, *varDecl)
-
+							name := specV.Names[i].Name
+							de.register(pkgName, name, *declVar)
+						// 複合リテラルの場合
 						case *ast.CompositeLit:
-							// 構造体リテラルの型が SelectorExpr
-							if selExpr, ok := rhs.Type.(*ast.SelectorExpr); ok {
-								pkgName := selExpr.X.(*ast.Ident).Name
-								structDecl := &declStruct{
-									typeName: selExpr.Sel.Name,
+							switch valTypeExprV := valExprV.Type.(type) {
+							// セレクタ式の場合
+							case *ast.SelectorExpr:
+								pkgName := valTypeExprV.X.(*ast.Ident).Name
+								declStruct := &declStruct{
+									typeName: valTypeExprV.Sel.Name,
 								}
-								name := valSpec.Names[i].Name
-								de.register(pkgName, name, *structDecl)
+								name := specV.Names[i].Name
+								de.register(pkgName, name, *declStruct)
 							}
+						// 演算子つきの場合
 						case *ast.UnaryExpr:
-							if rhs.Op == token.AND {
-								// & 演算子の場合
-								if compLit, ok := rhs.X.(*ast.CompositeLit); ok {
-									if selExpr, ok := compLit.Type.(*ast.SelectorExpr); ok {
-										pkgName := selExpr.X.(*ast.Ident).Name
-										structDecl := &declStruct{
-											typeName: selExpr.Sel.Name,
+							switch valExprV.Op {
+							// & 演算子の場合
+							case token.AND:
+								switch valExprV := valExprV.X.(type) {
+								// 複合リテラルの場合s
+								case *ast.CompositeLit:
+									switch valExprV := valExprV.Type.(type) {
+									case *ast.SelectorExpr:
+										pkgName := valExprV.X.(*ast.Ident).Name
+										declStruct := &declStruct{
+											typeName: valExprV.Sel.Name,
 										}
-										name := valSpec.Names[i].Name
-										de.register(pkgName, name, *structDecl)
+										name := specV.Names[i].Name
+										de.register(pkgName, name, *declStruct)
 									}
 								}
 							}
+						// 関数呼び出しの場合
 						case *ast.CallExpr:
-							// 関数の戻り値を代入している場合
-							if selExpr, ok := rhs.Fun.(*ast.SelectorExpr); ok {
-								pkgName := selExpr.X.(*ast.Ident).Name
-								for i, name := range valSpec.Names {
+							switch funExprV := valExprV.Fun.(type) {
+							case *ast.SelectorExpr:
+								// パッケージ名付きの関数呼び出し (pkg.Func())
+								pkgName := funExprV.X.(*ast.Ident).Name
+								funcName := funExprV.Sel.Name
+								for j, nameIdent := range specV.Names {
 									funcDecl := &declFunc{
-										name:  selExpr.Sel.Name,
-										order: i,
+										name:  funcName,
+										order: j,
 									}
-									de.register(pkgName, name.Name, *funcDecl)
+									de.register(pkgName, nameIdent.Name, *funcDecl)
 								}
 							}
 						}
@@ -148,6 +183,15 @@ func (de *DeclEntry) Register(input string) error {
 		}
 	}
 	return nil
+}
+
+func (de *DeclEntry) receiverTypePkgName(receiverName string) string {
+	for _, decl := range *de.decls {
+		if decl.Name() == receiverName {
+			return decl.Pkg()
+		}
+	}
+	return ""
 }
 
 func (de *DeclEntry) Decls() []decl {
@@ -191,13 +235,4 @@ func (de *DeclEntry) register(pkg, name string, rhs any) {
 		})
 	default:
 	}
-}
-
-func (de *DeclEntry) receiverTypePkgName(receiverName string) string {
-	for _, decl := range *de.decls {
-		if decl.Name() == receiverName {
-			return decl.Pkg()
-		}
-	}
-	return ""
 }
