89a0da5804b90deafa6f07694151622e7d730cb9
YutaKakiki <uta31501224@gmail.com>
2025-08-03 03:34:47 +0900

    ♻️ Add err handling

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	"log"
-
 	"github.com/kakkky/gonsole/completer"
 	"github.com/kakkky/gonsole/decls"
+	"github.com/kakkky/gonsole/errs"
 	"github.com/kakkky/gonsole/executor"
 	"github.com/kakkky/gonsole/repl"
 )
 
 func main() {
 	candidates, err := completer.NewCandidates(".")
-	declEntry := decls.NewDeclEntry()
 	if err != nil {
-		log.Fatalf("failed to create candidates: %v", err)
+		errs.HandleError(err)
 	}
-	repl := repl.NewRepl(completer.NewCompleter(candidates, declEntry), executor.NewExecutor(declEntry))
+	declEntry := decls.NewDeclEntry()
+	executor, err := executor.NewExecutor(declEntry)
+	if err != nil {
+		errs.HandleError(err)
+	}
+	completer := completer.NewCompleter(candidates, declEntry)
+	repl := repl.NewRepl(completer, executor)
 	repl.Run()
 }
