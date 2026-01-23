8c972711242ef3284be23cee37de9c2acf641fe0
YutaKakiki <uta31501224@gmail.com>
2026-01-24 04:52:20 +0900

    ♻️ Define version package

---
+++ version/version.go
@@ -0,0 +1,55 @@
+package version
+
+import (
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/kakkky/gonsole/errs"
+)
+
+const VERSION = "v1.3"
+
+func PrintVersion() {
+	fmt.Println("   " + VERSION)
+}
+
+type relesasesInfoResponse struct {
+	LatestVersion string `json:"tag_name"`
+}
+
+func IsLatestVersion() (bool, string, error) {
+	latestVersion, err := fetchLatestVersion()
+	if err != nil {
+		return false, "", err
+	}
+	return latestVersion == VERSION, latestVersion, nil
+}
+
+func fetchLatestVersion() (string, error) {
+	resp, err := http.Get("https://api.github.com/repos/kakkky/gonsole/releases/latest")
+	if err != nil {
+		return "", errs.NewInternalError("failed to fetch latest release").Wrap(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", errs.NewInternalError("failed to read response body").Wrap(err)
+	}
+	var releasesInfo relesasesInfoResponse
+	if err := json.Unmarshal(body, &releasesInfo); err != nil {
+		return "", errs.NewInternalError("failed to unmarshal response body").Wrap(err)
+	}
+
+	return releasesInfo.LatestVersion, nil
+}
+
+//go:embed latest_ver_note_ascii.txt
+var latestVerNoteAscii []byte
+
+func PrintNoteLatestVersion(latestVersion string) {
+	fmt.Printf(string(latestVerNoteAscii), latestVersion, VERSION)
+}
