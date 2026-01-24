6e799777895d6c29f15ae2af8316828208f1d965
YutaKakiki <uta31501224@gmail.com>
2026-01-24 17:38:41 +0900

    🚨 Ignore lintcheck for ast.Package

--- types/types.go
+++ types/types.go
@@ -9,6 +9,8 @@ type PkgName string
 
 // GoAstNodes は Go の抽象構文木ノードの集合を表す。
 // キーはパッケージ名、値はそのパッケージに属する ast.Package ノードのスライス.
+//
+// nolint:staticcheck // 定義されている変数名、関数名など名前だけに関心があるため、*ast.Packageだけで十分
 type GoAstNodes map[PkgName][]*ast.Package
 
 // DeclName は宣言名を表す。
@@ -20,7 +22,7 @@ type StructFieldName string
 // TypeName は型名を表す。
 type TypeName string
 
-// ReceiverTypeName はメソッドのレシーバ型名を表す。
+// ReceiverType==Name はメソッドのレシーバ型名を表す。
 type ReceiverTypeName TypeName
 
 // ImportPath はインポートパスを表す。
