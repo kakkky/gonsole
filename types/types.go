9cf3652dd2f4fb4f1c2ac6b570d1ca877b69c1a4
YutaKakiki <uta31501224@gmail.com>
2026-02-14 01:01:41 +0900

    🔥 Remove unneccesary type

--- types/types.go
+++ types/types.go
@@ -1,17 +1,7 @@
 package types
 
-import (
-	"go/ast"
-)
-
 // PkgName はパッケージ名を表す。
 type PkgName string
-
-// GoAstNodes は Go の抽象構文木ノードの集合を表す。
-// キーはパッケージ名、値はそのパッケージに属する ast.Package ノードのスライス.
-//
-// nolint:staticcheck // 定義されている変数名、関数名など名前だけに関心があるため、*ast.Packageだけで十分
-type GoAstNodes map[PkgName][]*ast.Package
 
 // DeclName は宣言名を表す。
 type DeclName string
