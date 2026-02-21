6582a7b9ed46e1537e52b32c36f4ff4e7a005133
YutaKakiki <uta31501224@gmail.com>
2026-02-21 13:28:18 +0900

    ♻️ Remove parsing logic from Register()

--- declregistry/decl_registry.go
+++ declregistry/decl_registry.go
@@ -1,15 +1,9 @@
 package declregistry
 
 import (
-	"go/ast"
-	"slices"
-	"strings"
-
 	gotypes "go/types"
 
-	"github.com/kakkky/gonsole/errs"
 	"github.com/kakkky/gonsole/types"
-	"golang.org/x/tools/go/packages"
 )
 
 // DeclRegistry はReplセッション中に宣言された変数の情報を管理する
@@ -24,171 +18,39 @@ func NewRegistry() *DeclRegistry {
 	}
 }
 
-// テストコードでRegisterの呼び出しをスキップするためのフラグ
-var SkipRegisterMode bool
-
 // Register は入力された最後の文を解析して、宣言された変数の情報をDeclRegistryに登録する
-func (dr *DeclRegistry) Register(tmpFileName string) error {
-	// テストコードでの呼び出しをスキップするためのフラグ
-	// 見通しは悪いが、一旦これで対応
-	if SkipRegisterMode {
-		return nil
-	}
+func (dr *DeclRegistry) Register(declName types.DeclName, declType gotypes.Type) error {
+	var typeName types.TypeName
+	var typePkgName types.PkgName
+	var pointered bool
 
-	cfg := &packages.Config{
-		Mode: packages.NeedName | packages.NeedTypes | packages.NeedTypesInfo | packages.NeedSyntax,
-		Dir:  "",
-	}
-
-	pkgs, err := packages.Load(cfg, tmpFileName)
-	if err != nil || len(pkgs) == 0 {
-		return errs.NewInternalError("failed to load package").Wrap(err)
-	}
-
-	pkg := pkgs[0]
-
-	pkg.Errors = slices.DeleteFunc(pkg.Errors, func(err packages.Error) bool {
-		switch {
-		case strings.Contains(err.Msg, "declared and not used"):
-			return true
-		case strings.Contains(err.Msg, "imported and not used"):
-			return true
-		case strings.Contains(err.Msg, "undefined"):
-			return true
+	switch declTypeV := declType.(type) {
+	case *gotypes.Named:
+		typeName = types.TypeName(declTypeV.Obj().Name())
+		if declTypeV.Obj().Pkg() != nil {
+			typePkgName = types.PkgName(declTypeV.Obj().Pkg().Name())
 		}
-		return false
-	})
-
-	if len(pkg.Errors) > 0 {
-		var errMsgs []string
-		for _, pkgErr := range pkg.Errors {
-			errMsgs = append(errMsgs, pkgErr.Msg)
-		}
-		return errs.NewBadInputError("failed to parse input: " + strings.Join(errMsgs, "; "))
-	}
-
-	var mainFunc *ast.FuncDecl
-	for _, decl := range pkg.Syntax[0].Decls {
-		funcDecl, ok := decl.(*ast.FuncDecl)
-		if ok && funcDecl.Name.Name == "main" {
-			mainFunc = funcDecl
-			break
-		}
-	}
-	if mainFunc == nil {
-		return errs.NewBadInputError("main function not found")
-	}
-	mainFuncBodyList := mainFunc.Body.List
-	mainFuncBodyList = slices.DeleteFunc(mainFuncBodyList, func(stmt ast.Stmt) bool {
-		assignStmt, ok := stmt.(*ast.AssignStmt)
-		if !ok {
-			return false
-		}
-		for _, stmtLHS := range assignStmt.Lhs {
-			lhsIdent, ok := stmtLHS.(*ast.Ident)
-			if !ok {
-				continue
-			}
-			if lhsIdent.Name == "_" {
-				return true
-			}
-		}
-		return false
-	})
-
-	lastStmt := mainFuncBodyList[len(mainFuncBodyList)-1]
-
-	switch lastStmtV := lastStmt.(type) {
-	case *ast.AssignStmt:
-		dr.registerAssimentStmt(lastStmtV, pkg.TypesInfo)
-	case *ast.DeclStmt:
-		dr.registerDeclStmt(lastStmtV, pkg.TypesInfo)
-	}
-	return nil
-}
-
-func (dr *DeclRegistry) registerAssimentStmt(assignmentStmt *ast.AssignStmt, typesInfo *gotypes.Info) {
-	for _, stmtLHS := range assignmentStmt.Lhs {
-		lhsIdent, ok := stmtLHS.(*ast.Ident)
-		if !ok {
-			continue
-		}
-		typ := typesInfo.TypeOf(stmtLHS)
-		var typeName types.TypeName
-		var typePkgName types.PkgName
-		var pointered bool
-
-		switch typV := typ.(type) {
+	case *gotypes.Pointer:
+		pointered = true
+		switch pointeredTypV := declTypeV.Elem().(type) {
 		case *gotypes.Named:
-			typeName = types.TypeName(typV.Obj().Name())
-			if typV.Obj().Pkg() != nil {
-				typePkgName = types.PkgName(typV.Obj().Pkg().Name())
-			}
-		case *gotypes.Pointer:
-			pointered = true
-			switch pointeredTypV := typV.Elem().(type) {
-			case *gotypes.Named:
-				typeName = types.TypeName(pointeredTypV.Obj().Name())
-				if pointeredTypV.Obj().Pkg() != nil {
-					typePkgName = types.PkgName(pointeredTypV.Obj().Pkg().Name())
-				}
-			default:
-				typeName = types.TypeName(pointeredTypV.String())
+			typeName = types.TypeName(pointeredTypV.Obj().Name())
+			if pointeredTypV.Obj().Pkg() != nil {
+				typePkgName = types.PkgName(pointeredTypV.Obj().Pkg().Name())
 			}
 		default:
-			typeName = types.TypeName(typ.String())
+			typeName = types.TypeName(pointeredTypV.String())
 		}
-		dr.register(Decl{
-			Name:        types.DeclName(lhsIdent.Name),
-			Pointered:   pointered,
-			TypeName:    typeName,
-			TypePkgName: typePkgName,
-		})
+	default:
+		typeName = types.TypeName(declType.String())
 	}
-}
-
-func (dr *DeclRegistry) registerDeclStmt(declStmt *ast.DeclStmt, typesInfo *gotypes.Info) {
-	switch stmtDeclV := declStmt.Decl.(type) {
-	case *ast.GenDecl:
-		for _, stmtDeclSpec := range stmtDeclV.Specs {
-			switch stmtDeclSpecV := stmtDeclSpec.(type) {
-			case *ast.ValueSpec:
-				for _, name := range stmtDeclSpecV.Names {
-					typ := typesInfo.TypeOf(name)
-					var typeName types.TypeName
-					var typePkgName types.PkgName
-					var pointered bool
-
-					switch typV := typ.(type) {
-					case *gotypes.Named:
-						typeName = types.TypeName(typV.Obj().Name())
-						if typV.Obj().Pkg() != nil {
-							typePkgName = types.PkgName(typV.Obj().Pkg().Name())
-						}
-					case *gotypes.Pointer:
-						pointered = true
-						switch pointeredTypV := typV.Elem().(type) {
-						case *gotypes.Named:
-							typeName = types.TypeName(pointeredTypV.Obj().Name())
-							if pointeredTypV.Obj().Pkg() != nil {
-								typePkgName = types.PkgName(pointeredTypV.Obj().Pkg().Name())
-							}
-						default:
-							typeName = types.TypeName(pointeredTypV.String())
-						}
-					default:
-						typeName = types.TypeName(typ.String())
-					}
-					dr.register(Decl{
-						Name:        types.DeclName(name.Name),
-						Pointered:   pointered,
-						TypeName:    typeName,
-						TypePkgName: typePkgName,
-					})
-				}
-			}
-		}
-	}
+	dr.register(Decl{
+		Name:        declName,
+		Pointered:   pointered,
+		TypeName:    typeName,
+		TypePkgName: typePkgName,
+	})
+	return nil
 }
 
 func (dr *DeclRegistry) register(decl Decl) {
